4af686a077e16ecc87f64ab907f487cba87acda7
ManashSwain <manash0802@gmail.com>
2025-05-14 21:33:34 +0530

    implement: theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import Layout from './components/layout';
+import { ThemeProvider } from './context/theme-provider';
 
 function App() {
   return (
     <>
      <BrowserRouter>
+     <ThemeProvider defaultTheme="dark">
      <Layout>  
         Hello
      </Layout>
+     </ThemeProvider>
      </BrowserRouter>
   
 
