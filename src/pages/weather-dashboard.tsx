9dbf3c999a52e2932934add1f88a843157438e9f
ManashSwain <manash0802@gmail.com>
2025-05-15 17:02:40 +0530

    implement: ui design for weather details

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -3,6 +3,7 @@ import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button"
+import WeatherDetails from "@/components/weather-details";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hooks/use-weather";
 import { AlertTriangle, MapPin, RefreshCw } from "lucide-react"
@@ -104,6 +105,7 @@ const weatherDashboard = () => {
     </div>
     <div>
         {/* details  */}
+        <WeatherDetails data={weatherQuery.data}/>
         {/* forecast  */}
     </div>
   </div>
